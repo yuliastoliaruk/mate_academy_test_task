1b5695de34344d5cfd38ee027c11b86bf7414956
yuliiastoliaruk <uliastolaruk@gmail.com>
2025-03-10 15:04:08 +0200

    Updated finnal result

--- collect_courses_data.py
+++ collect_courses_data.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 import pandas as pd
 
 web = "https://mate.academy/"
@@ -26,20 +26,33 @@ for link in course_links:
     driver.get(link)
 
     # extract course type
-    buttons = driver.find_elements(By.XPATH, '//a[contains(@class, "ComparisonTable_comparisonButton")]')
     full_time = False
     flex = False
+    buttons = driver.find_elements(By.XPATH, '//span[contains(@class, "ButtonBody_buttonText")]')
+
     for button in buttons:
-        text = button.text.strip()
-        if "Навчатися повний день" in text:
-            full_time = True
-        if "Навчатися у вільний час" in text:
-            flex = True
+        try:
+            text = button.text.strip()
+            if "Навчатися повний день" in text:
+                full_time = True
+            if "Навчатися у вільний час" in text:
+                flex = True
+        except StaleElementReferenceException:
+            continue
 
     course_type.append("/".join(filter(None, ["FULL-TIME" if full_time else "", "FLEX" if flex else ""])))
 
-    description.append(driver.find_element(By.XPATH, '//pre[contains(@class, "SalarySection_aboutProfession")]').text)
-    topics_count.append(driver.find_element(By.XPATH,'//div[contains(@class, "CourseProgram_purple__STsuM")]//p[contains(@class, "FactBlock_factNumber__d_8nn")]').text)
+    description.append(driver.find_element(By.CSS_SELECTOR, 'p.SalarySection_aboutProfession__C6ftM').text)
+
+    # extract number of topics
+    topic_elements = driver.find_elements(By.XPATH, '//p[contains(@class, "CourseModulesList_topicsCount__H_fv3")]')
+    total_topics = 0
+    for topic in topic_elements:
+        topics_text = topic.text
+        topics_number = ''.join(filter(str.isdigit, topics_text))
+        if topics_number:
+            total_topics += int(topics_number)
+    topics_count.append(str(total_topics))
 
     # extract number of modules
     try:
